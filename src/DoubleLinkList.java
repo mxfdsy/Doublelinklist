4642090e4d35a4096f7beb383730f67221beb689
平凡的世界 <963559210@qq.com>
2018-03-28 23:08:16 +0800

    删除有问题

--- src/DoubleLinkList.java
+++ src/DoubleLinkList.java
@@ -1,53 +1,111 @@
+import javax.xml.soap.Node;
+
 /**
+ * @author 没想发的岁月
  * 需求：编写一个双向链表
  * Created by 没想法的岁月 on 2018/3/27.
  */
 public class DoubleLinkList  {
-    //链表集合的第一个节点
-    private Node first;
-    //最后一个节点
-    private Node last;
+    //第一个节点
+    Node first;
+    //第二个节点
+    Node last;
+    //节点数
     int size;
 
-    public boolean search(Object ele) {
+    public Object  remove(Object ele) {
+        // 先找到这个要被删除的节点
         if (size == 0) {
-           return false;
+            return "当前的集合中没有任何的元素";
+        }else{
+            Node current = this.first;
+            for (int i = 0; i <size ; i++) {
+                if (!current.ele.equals(ele)) {
+                    if (current.next == null){
+                        return "集合中不存在含有此元素的节点";
+                    }
+                }
+                current = current.next;
+            }
+            //删除的节点
+            if (current == first){
+                current.next.prev =null;
+                current.next = first;
+            }else if (current == last){
+                current.prev.next =null;
+                current.prev =last;
+            }else{
+                current.prev =current.next.prev;
+                current.next= current.prev.next;
+            }
+            size--;
+            return "成功删除含有元素"+ current.ele + "的节点";
         }
-        for (int i = 0; i <size ; i++) {
-
-        }
-        return false;
     }
+
+    //使用类来描述 接点这类输入
     class Node{
-        Node pre;
+        //表示上一个节点
+        Node  prev;
+        //表示下一个节点
         Node next;
+        //当前节点中存在的元素
         Object ele;
 
         public Node(Object ele) {
             this.ele = ele;
         }
     }
-    public void addLast(Object ele) {
+    //获取当前的节点数
+    public  int getSize(){
+        return size;
+    }
+    //在第一个位置增加一个节点
+    public void addFirst(Object ele) {
         Node node = new Node(ele);
-        if (size == 0) {
-            Node first = node;
-            Node last = node;
-        }else {
-            this.last.next =node;
-            node.pre = this.last;
+        if (size == 0){
+            this.first = node;
+            this.last = node;
+        }else{
+            this.first.prev = node;
+            node.next =this.first;
+            this.first = node;
         }
         size++;
     }
 
-    public void addFirst(Object ele) {
+    //在最后添加一个节点
+    public void addLast(Object ele) {
         Node node = new Node(ele);
-        if (size == 0) {
-            Node first = node;
-            Node last = node;
+        if (size == 0){
+            this.first =node;
+            this.last = node;
         }else{
-            node.next = this.first;
-            this.first.pre =node;
+            this.last.next= node;
+            node.prev = this.last;
+            this.last =node;
         }
         size++;
+    }
+    //list的toString
+    @Override
+    public String toString(){
+        if (size == 0) {
+            return "[]";
+        }else{
+            Node current = this.first;
+           StringBuilder sb = new StringBuilder(size*2-1);
+           sb.append("[");
+            for (int i = 0; i <size ; i++) {
+                sb.append(current.ele);
+                if (i!=size-1) {
+                    sb.append(",");
+                }else {
+                    sb.append("]");
+                }
+                current = current.next;
+            }
+            return sb.toString();
+        }
     }
 }
--- src/DoubleLinkListDmeo.java
+++ src/DoubleLinkListDmeo.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 没想法的岁月 on 2018/3/27.
  */
-public class DoubleLinkListDmeo extends  DoubleLinkList{
+public class DoubleLinkListDmeo {
     public static void main(String[] args) {
-        DoubleLinkListDmeo  List =new DoubleLinkListDmeo();
-        List.addLast("A");
-        List.addFirst("B");
-        List.addFirst("B");
-        List.search("A");
-        System.out.println(List.size);
+        DoubleLinkList list = new DoubleLinkList();
+        //增加第一个节点
+        list.addFirst("A");
+        //获取当前的节点数
+        System.out.println(list.getSize());
+        // 增加最后一个节点
+        list.addLast("B");
+        list.addLast("C");
+        list.addLast("D");
+        list.addLast("E");
+
+        System.out.println(list.getSize());
+        //打印出当前节点的toString
+        System.out.println(list);
+        //删除带有这个元素的节点且返回这个值
+        System.out.println(list.remove("A"));
+        System.out.println(list);
     }
 }
